3034406afc79bf7b271798b01285511d7d1d6660
Martisni <martahusillos1998@gmail.com>
2021-04-20 01:15:16 +0200

    Visualizar objetos creados

--- src/main/java/com/sanvalero/Application.java
+++ src/main/java/com/sanvalero/Application.java
@@ -1,4 +1,8 @@
 package com.sanvalero;
 
 public class Application {
+    public static void main(String[] args) {
+        Menuprincipal menu = new Menuprincipal();
+        menu.menuInicio();
+    }
 }
